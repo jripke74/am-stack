d5df10b1978003f273c5f712acd0a9d04f0a2dc5
jripke74 <jripke74@gmail.com>
2025-10-27 10:38:54 -0700

    add new Stack() and console.log()

--- app.js
+++ app.js
@@ -14,4 +14,24 @@ class Stack {
   isEmpty() {
     return this.items.length === 0;
   }
+
+  toArray() {
+    return this.items.slice();
+  }
 }
+
+const stack = new Stack();
+stack.push("Cook dinner!");
+stack.push("Wash the dishes!");
+stack.push("Buy ingredients!");
+
+console.log(stack.toArray());
+
+console.log(stack.pop());
+
+console.log(stack.pop());
+
+console.log(stack.pop());
+console.log(stack.pop());
+
+console.log(stack.toArray());
